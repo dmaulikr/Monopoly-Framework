26266310c5ea1ea6e39677a9d4c9a9b23bbb3670
Étienne Beaulé <beauleetienne0@gmail.com>
2015-09-05 23:13:40 -0300

    Added methods for Player, Space, and Card
    Taken from another of my projects.
    
    Also has many empty methods for tests.

--- Monopoly.swift
+++ Monopoly.swift
@@ -7,82 +7,356 @@
 
 import Foundation
 
-class Player {
-	var name: String
+/* Make Space & Player Equatable
+* Needed to differentiate between community chest and chance spaces
+*/
+func == (lhs: Space, rhs: Space) -> Bool { // Space
+	var isEqual: Bool = false
+	if lhs.name == rhs.name && lhs.value == rhs.value { // Name and (arbitrary) value
+		isEqual = true
+	}
+	return isEqual
+}
 
-	init(name: String) {
-		self.name = name
+func == (lhs: Player, rhs: Player) -> Bool { // Player
+	var isEqual = false
+	if lhs.id == rhs.id {
+		isEqual = true
+	}
+	return isEqual
+}
+
+class Player: Equatable {
+	var name: String = "" // Name of player
+	var id: Int // Turn #
+
+	init(name: String, id: Int) {
+		self.name = name // Default would be ("Player #")
+		self.id = id
 	}
 
-	private var funds: Int = 1500 // Amount of money not including properties
+	private var money: Int = startingMoney // Current amount of money
 
-	func removeFunds(amount: Int) {
-		if funds > amount {
-			funds -= amount
+	private var playerInJail: Bool = false // Diff. <-> just visiting and in jail
+	var playerHasFreeCard: [Card.format] = [] // Get out of jail free
+	func isInJail() -> Bool {
+		return playerInJail
+	}
+
+	var currentSpace: Space = start {
+		didSet {
+			currentSpace.whenPlayerOnSpace(self) // Perform space action
 		}
 	}
-	func addFunds(amount: Int) {
-		funds += amount
+
+	func removeMoney(amount: Int) {
+		if money < amount {
+			/*// Check all possesions
+			var combinedWealth
+			for space in spaceArray {
+			if
+			}*/
+			/// TODO: Check all possesions, if < amount, bankrupt and switch owner
+			/// TODO: Give player option
+		} else {
+			money -= amount
+		}
 	}
+
+	func addMoney(amount: Int) {
+		money += amount
+	}
+
 	func getFunds() -> Int {
-		return funds
+		return money
+	}
+
+	func sendToJail() {
+		playerInJail = true
+		if playerHasFreeCard.isEmpty != true {
+			playerInJail = false
+			playerHasFreeCard.removeFirst() // Remove free card, no diff between type
+		}
+		currentSpace = jailVisit
+	}
+
+	// On turn if doubles thrown three times, send to jail
+	func onTurn() {
+		var dice = diceRoll()
+
+		func moveSpace() {
+			let i: Int? = spaceArray.indexOf(currentSpace)
+			currentSpace = spaceArray[(dice.die1 + dice.die2 + i!) % spaceArray.count]
+		}
+
+		var doublesCount: Int = 0 {
+			willSet {
+				if doublesCount == 2 {
+					sendToJail()
+				} else {
+					moveSpace()
+				}
+			}
+			didSet {
+				moveSpace()
+				dice = diceRoll() // If doubles, roll again
+			}
+		}
+
+		if dice.die1 == dice.die2 {
+			doublesCount++
+		} else {
+			moveSpace()
+		}
 	}
 }
 
-class Space {
+class Space: Equatable {
+	enum Action: Int {
+		case onProperty = 0
+		case onTaxes = 1
+		case onCommunityChest = 2
+		case onChance = 3
+		case toJail = 4
+	}
+
 	var name: String = ""
+	var action: Action?
+	var value: Int?
 
-	init(name: String) {
+	init(name: String, action: Action?, value: Int? = nil) {
 		self.name = name
+		self.action = action
+		self.value = value
 	}
 
-	var rentPrice: Int { // Money to remove when on space. For taxes
-		return 0
+	func whenPlayerOnSpace(player: Player) {
+		if action != nil {
+			switch action! {
+			case .onProperty:
+				break
+			case .onTaxes:
+				player.removeMoney(value!)
+			case .onCommunityChest:
+				Card.drawCard(.communityChest)
+			case .onChance:
+				Card.drawCard(.chance)
+			case .toJail:
+				player.sendToJail()
+			}
+		}
 	}
 }
 
-// Includes Utilities/Railroads
 class OwnedSpace: Space {
 	enum Site: String {
 		case Railroad = "Railroad"
 		case Utility = "Utility"
+		case Property = "Property"
 	}
 
-	var spaceType: Site
+	var owner: Player? = nil {
+		didSet {
+			if isMortgaged {
+				let mortgageChange = onMortgageChangeChoice() // User option
+				if mortgageChange {
+					deMorgageSpace()
+				} else {
+					owner?.removeMoney(mortgagePrice / 10) // Remove 10%
+				}
+			}
+		}
+	}
+	var type: Site
 
-	init(name: String, spaceType: Site) {
-		self.spaceType = spaceType
-		super.init(name: name)
+	init(name: String, type: Site) {
+		self.type = type
+
+		super.init(name: name, action: Action.onProperty, value: nil)
 	}
 
 	var price: Int {
 		var price = 0
 
-		switch spaceType {
+		switch type {
 		case .Railroad:
-			price = 200
+			price = railPrice
 		case .Utility:
-			price = 150
+			price = utilityPrice
+		case .Property:
+			break
 		}
 		return price
 	}
+	var rentPrice: Int {
+		var price = 0
+		switch type {
+		case .Railroad:
+			func isOwnedBySelf(space: Space) -> Bool {
+				var isApplic = false
+				let owned: OwnedSpace = space as! OwnedSpace
+				if (owned.type == .Railroad) && (owned.owner == self.owner){
+					isApplic = true
+				}
+				return isApplic
+			}
+			let filteredArray = spaceArray.filter(isOwnedBySelf)
 
-	override var rentPrice: Int {
-		var price: Int = 0
-		switch spaceType {
-		case .Railroad: // Variable rent by # owned
-			break
-		case .Utility: //Variable rent by dice roll & # owned
-			let dice = rollDice()
-			price = (dice.0 + dice.1) * 40
+			switch filteredArray.count {
+			case 1:
+				price = 50
+			case 2:
+				price = 100
+			case 3:
+				price = 200
+			default:
+				price = 25
+			}
+		case .Utility:
+			let dice = diceRoll()
+
+			func isOwnedBySelf(space: Space) -> Bool {
+				var isApplic = false
+				let owned: OwnedSpace = space as! OwnedSpace
+				if (owned.type == .Utility) && (owned.owner == self.owner){
+					isApplic = true
+				}
+				return isApplic
+			}
+			let filteredArray = spaceArray.filter(isOwnedBySelf)
+
+			price = dice.die1 + dice.die2
+			if filteredArray.count == 1 {
+				price *= 10
+			} else {
+				price += 4
+			}
+		default:
+			price = 0
 		}
 		return price
+	}
+	var mortgagePrice: Int {
+		return price / 2
+	}
+
+	var isMortgaged: Bool = false
+	func mortgageSpace() {
+		owner?.addMoney(mortgagePrice)
+		isMortgaged = true
+	}
+
+	func deMorgageSpace() {
+		owner?.removeMoney(mortgagePrice + mortgagePrice / 10) // Price + 10%
+		isMortgaged = false
+	}
+
+	override func whenPlayerOnSpace(playerOn: Player) {
+		if (owner != playerOn) || (isMortgaged == false) {
+			playerOn.removeMoney(rentPrice)
+		}
 	}
 }
 
-func rollDice() -> (Int, Int) {
+class Property: OwnedSpace {
+	enum Group: Int {
+		case Brown = 0
+		case Light_Blue = 1
+		case Pink = 2
+		case Orange = 3
+		case Red = 4
+		case Yellow = 5
+		case Green = 6
+		case Blue = 7
+
+		var propNumber: Int {
+			switch self {
+			case .Brown, .Blue:
+				return 2 // Two properties in Brown or Blue
+			default:
+				return 3 // Three properties elsewhere
+			}
+		}
+	}
+
+	var group: Group
+	var groupPosition: Int
+
+	init(name: String, group: Group, groupPosition: Int) {
+		self.group = group
+		self.groupPosition = groupPosition
+
+		super.init(name: name, type: .Property)
+	}
+
+	private var numberOfHouses: UInt = 0
+	var housePrice: Int {
+		return 2
+	}
+
+	override var price: Int {
+		var price = group.rawValue * propAddingPrice + propStartingPrice
+
+		switch group {
+		case .Blue:
+			price += 10
+
+			if groupPosition == 2 {
+				price += 50
+			}
+		default:
+			if groupPosition == 3 {
+				price += propTopGroupPrice
+			}
+		}
+		return price
+	}
+	/*
+	override var rentPrice: Int {
+		return 0
+	}
+	*/
+	func buyHouse() {
+		if numberOfHouses < maxHouseNumber {
+			numberOfHouses += 1
+			owner?.removeMoney(housePrice)
+		}
+	}
+
+	func sellHouse() {
+		if numberOfHouses > 0 {
+			numberOfHouses -= 1
+			owner?.addMoney(housePrice / 2) // Half price
+		}
+	}
+}
+class Card {
+	enum format {
+		case communityChest
+		case chance
+	}
+
+	static func drawCard(type: format) {
+		switch type {
+		case .chance:
+			chanceCards
+		case .communityChest:
+			communityChestCards
+		}
+	}
+}
+
+
+// Provides one integer between 1 and 6 for each die (2)
+func diceRoll() -> (die1: Int, die2: Int) {
 	let die1 = Int(arc4random_uniform(6) + 1)
 	let die2 = Int(arc4random_uniform(6) + 1)
 
 	return (die1, die2)
+}
+
+// Extra functions
+
+func onMortgageChangeChoice() -> Bool {
+	return false
 }
--- MonopolyTests.swift
+++ MonopolyTests.swift
@@ -33,23 +33,115 @@ class MonopolyTests: XCTestCase {
     }
 
 	func testInsufficentFunds() {
-		let testPlayer = Player(name: "Test") // Funds set at 1500
-		testPlayer.removeFunds(1501) // > than 1500
+		let testPlayer = Player(name: "Test", id: 0) // Funds set at 1500
+		testPlayer.removeMoney(1501) // > than 1500
 
 		XCTAssertEqual(testPlayer.getFunds(), 1500) // Test if did nothing
 	}
 
 	func testRemoveFunds() {
-		let testPlayer = Player(name: "Test") // Funds set at 1500
-		testPlayer.removeFunds(500) // Funds become 1000
+		let testPlayer = Player(name: "Test", id: 0) // Funds set at 1500
+		testPlayer.removeMoney(500) // Funds become 1000
 
 		XCTAssertEqual(testPlayer.getFunds(), 1000)
 	}
 
 	func testAddFunds() {
-		let testPlayer = Player(name: "Test") // Funds set at 1500
-		testPlayer.addFunds(500) // Funds become 2000
+		let testPlayer = Player(name: "Test", id: 0) // Funds set at 1500
+		testPlayer.addMoney(500) // Funds become 2000
 
 		XCTAssertEqual(testPlayer.getFunds(), 2000)
+	}
+
+	func testSendToJail() {
+		let testPlayer = Player(name: "Test", id: 0)
+		testPlayer.currentSpace = start
+		testPlayer.sendToJail()
+
+		XCTAssert(testPlayer.isInJail())
+	}
+
+	func testSendToJailWithCard() {
+		let testPlayer = Player(name: "Test", id: 0)
+		testPlayer.playerHasFreeCard.append(.chance)
+		testPlayer.sendToJail()
+
+		XCTAssert(testPlayer.playerHasFreeCard.isEmpty)
+	}
+
+	func testOnTaxes() {
+
+	}
+
+	func testOnUnownedProperty() {
+
+	}
+
+	func testOnOwnedProperty() {
+
+	}
+
+	func testOnOwnedGroup() {
+
+	}
+	
+	func testOnUtility1Owned() {
+
+	}
+
+	func testOnUtility2Onwed() {
+
+	}
+
+	func testOnRail1Owned() {
+
+	}
+
+	func testOnRail3Owned() {
+
+	}
+
+	func testMortgageSpaceMoneyReturned() {
+
+	}
+
+	func testMortgageSpaceNoRent() {
+
+	}
+
+	func testMortgageSpace10() {
+
+	}
+
+	func testMortgageSpaceSold() {
+
+	}
+
+	func testBuyProperty() {
+
+	}
+
+	func testBuyHouseNotFullGroup() {
+
+	}
+
+	func testBuyHouseFullGroup() {
+
+	}
+
+	func testBuyHouseOverMax() {
+
+	}
+
+	func testRentPriceWHotel() {
+
+	}
+
+	func testDistributeHouse() {
+
+	}
+
+	func testSellHouse() {
+
 	}
 }
